e2df0ba464b12a21c7ad6c07dbc50b5a51c42245
jiyoung <jiyoung@DESKTOP-BGCLLFJ>
2022-04-29 18:09:51 +0900

    지영 언니 코드 바꾼거 4월 29일

--- src/movie/dao/MovieDAO.java
+++ src/movie/dao/MovieDAO.java
@@ -1,7 +1,19 @@
 package movie.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import movie.mybatis.DBService;
+
 public class MovieDAO {
 	
 	//영화 목록 전체 조회
+	public static List<String> movieList(){
+		SqlSession ss = DBService.getFactory().openSession();
+		List<String> list = ss.selectList("moviemake.movieSelectAll");
+		ss.close();
+		return list;
+	}
 
 }
